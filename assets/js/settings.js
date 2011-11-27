9751150bf482605eab8fd2dbb1904f59142d9211
Ryan Brodkin <ryan@glabstudios.com>
2011-11-26 20:13:56 -0800

    Renamed project to "Notifier Plus for Google Mail." New style for main popup. Minor functional enhancements. Still a few bugs to iron out.

---
+++ assets/js/settings.js
@@ -0,0 +1,59 @@
+﻿var Settings = function() {
+
+   var prefix = 'mailchecker_';
+
+   var defaults = {
+      "poll": 15000,
+      "dn_timeout": 15000,
+      "language": "en",
+      "sn_audio": "chime.mp3",
+      "check_label": "",
+      "open_label": "#inbox",
+      "icon_set": "set1",
+      "preview_setting": 2,
+      "show_notification": true,
+      "check_gmail_off": false,
+      "hide_count": false,
+      "sound_off": false,
+      "show_notification": true,
+      "showfull_read": false,
+      "animate_off": false,
+      "open_tabs": false,
+      "no_mailto": false,
+      "archive_read": true,
+   };
+
+   Settings.reset = function () {
+      localStorage.clear();
+      console.log('Cleared local storage.');
+   };
+
+   Settings.read = function (key) {
+
+      var value = localStorage.getItem(prefix + key);
+
+      if (value != null) {
+         //console.log('Read: ' + key + '=' + value);
+         // Return value
+         if (value == 'true') return true;
+         else if (value == 'false') return false;
+         else return value;
+      } else {
+         // Key not found, store default value
+         if (defaults[key] != null) {
+            //console.log('Default: ' + key + '=' + defaults[key]);
+            this.store(key, defaults[key]);
+            return defaults[key];
+         }
+      }
+
+      return null;
+   };
+
+   Settings.store = function (key, value) {
+      localStorage.setItem(prefix + key, value);
+      console.log('Stored: ' + key + '=' + value);
+   };
+}
+
+Settings();

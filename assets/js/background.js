c0722f5a09d661420d0fe26dfe76e10bfe590829
Ryan Brodkin <ryan@glabstudios.com>
2011-12-19 21:54:09 -0800

    Sending Extension Version to GA as Custom Variable

--- assets/js/background.js
+++ assets/js/background.js
@@ -39,6 +39,12 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-27460318-2']);
 _gaq.push(['_trackPageview']);
+_gaq.push(['_setCustomVar',
+   2, // Index
+   'Version', // Name
+   chrome.app.getDetails().version, // Value
+   1 // Scope (1=Visitor-Level)
+]);
 
 (function() {
  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
